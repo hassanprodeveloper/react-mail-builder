1dd5de7d9e2b021734afe502842f406443349aa4
hassanprodeveloper <hassanprodeveloper@gmail.com>
2024-01-18 22:23:40 +0500

    updated import json page UI

--- src/components/editor/chooseJson/styled.ts
+++ src/components/editor/chooseJson/styled.ts
@@ -7,14 +7,20 @@ const Main = styled.div`
 	height: 100%;
 	justify-content: center;
 	align-items: center;
+	padding: 16px
+`
+const SVG = styled.div`
+	max-width: 100px;
+	max-height: 100px;
 `
 
 const Title = styled.h1`
 	font-size: 24px;
 	font-weight: 600;
-`
-
-const ChooseButton = styled.label`
+	text-align: center;
+	`
+	
+	const ChooseButton = styled.label`
 	all: unset;
 	font-size: 16px;
 	padding: 16px;
@@ -24,6 +30,7 @@ const ChooseButton = styled.label`
 	font-weight: 500;
 	cursor: pointer;
 	margin-top: 16px;
+	text-align: center;
 `
 
-export default { Main, Title, ChooseButton }
+export default { Main, Title, ChooseButton, SVG }
